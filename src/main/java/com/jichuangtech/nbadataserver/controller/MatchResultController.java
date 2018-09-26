12199820c12c25cf159970bdba9f7aab931820ef
huangbin <575395982@qq.com>
2018-09-27 00:26:59 +0800

    ADD [赛程] 添加根据日期来查询赛程的方法

--- src/main/java/com/jichuangtech/nbadataserver/controller/MatchResultController.java
+++ src/main/java/com/jichuangtech/nbadataserver/controller/MatchResultController.java
@@ -7,10 +7,7 @@ import com.jichuangtech.nbadataserver.model.Response;
 import com.jichuangtech.nbadataserver.model.vo.MatchResultRespVo;
 import com.jichuangtech.nbadataserver.repository.MatchResultRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,12 +20,26 @@ public class MatchResultController extends BaseController {
     private MatchResultRepository mMatchResultRepository;
 
     @Deprecated
-    @RequestMapping(value = "/{date}", method = RequestMethod.GET)
-    public Response<List<MatchResultRespVo>> listByDate(@PathVariable String date) {
+    @RequestMapping(value = "/byDate", method = RequestMethod.GET)
+    public Response<List<MatchResultRespVo>> listByDate(@RequestParam String date) {
         LOGGER.info(" listByDate date: " + date);
         Response<List<MatchResultRespVo>> response = new Response<>();
         
         response.data = mapList(mMatchResultRepository.findByGamedate(date), MatchResultRespVo.class);
+        LOGGER.info(" match result vo: " + response.data);
+        if(response.data == null) {
+            response.setStatusCode(ResponseCode.MATCH_RESULT_NOT_FOUND_CODE);
+        }
+        return response;
+    }
+
+    @Deprecated
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Response<MatchResultRespVo> listById(@PathVariable int id) {
+        LOGGER.info(" listById date: " + id);
+        Response<MatchResultRespVo> response = new Response<>();
+
+        response.data = mapSingle(mMatchResultRepository.findById(id), MatchResultRespVo.class);
         LOGGER.info(" match result vo: " + response.data);
         if(response.data == null) {
             response.setStatusCode(ResponseCode.MATCH_RESULT_NOT_FOUND_CODE);
